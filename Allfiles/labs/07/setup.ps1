0a9ee497cee39209c8a1d2fecd96869592844022
venkatavivekghanta <113364335+venkatvvg@users.noreply.github.com>
2023-04-13 10:31:56 -0400

    Update setup.ps1

--- Allfiles/labs/07/setup.ps1
+++ Allfiles/labs/07/setup.ps1
@@ -94,7 +94,7 @@ foreach ($provider in $provider_list){
 }
 
 # Generate unique random suffix
-[string]$suffix =  -join ((48..57) + (97..122) | Get-Random -Count 7 | % {[char]$_})
+[string]$suffix =  "$ucid"
 Write-Host "Your randomly-generated suffix for Azure resources is $suffix"
 $resourceGroupName = "dp203-07-$suffix"
 
